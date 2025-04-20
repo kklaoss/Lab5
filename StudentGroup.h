4a31608bad29d853588e1b828e6aa8054cfad1f7
Nikolay Usenko <klaos.usn@gmail.com>
2025-04-20 22:23:36 +0300

    Add Student.h

--- StudentGroup.h
+++ StudentGroup.h
@@ -1,23 +1,14 @@
-// Защита от многократного включения заголовочного файла
 #ifndef STUDENTGROUP_H
 #define STUDENTGROUP_H
-// Подключение библиотеки векторов
 #include <vector>
-// Подключение заголовочного файла класса Student
 #include "Student.h"
-// Объявление класса StudentGroup
 class StudentGroup
 {
 public:
-	// Метод для добавления студента в группу
 	void add_student(const Student& student);
-	// Метод для удаления студента из группы по фамилии
 	void remove_student(const std::string& last_name);
-	// Метод для отображения всех студентов группы
 	void display_all() const;
 private:
-	// Вектор для хранения студентов группы
 	std::vector<Student> students;
 };
-// Завершение блока защиты от многократного включения
 #endif
