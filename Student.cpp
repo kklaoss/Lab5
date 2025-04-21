2b1e30e01ec031c9685488746e6fafb0196ec85f
Nikolay Usenko <klaos.usn@gmail.com>
2025-04-21 14:20:16 +0300

    Add Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,14 @@
+// Подключение заголовочного файла класса Student
+#include "Student.h"
+// Реализация конструктора класса Student с инициализацией полей
+Student::Student(const std::string& last_name,
+	const std::string& birth_date,
+	const std::string& phone)
+	: last_name(last_name), birth_date(birth_date), phone(phone) {
+}
+// Реализация метода получения фамилии студента
+std::string Student::get_last_name() const { return last_name; }
+// Реализация метода получения даты рождения студента
+std::string Student::get_birth_date() const { return birth_date; }
+// Реализация метода получения телефона студента
+std::string Student::get_phone() const { return phone; }
